22a2a494aca26b77f9120627f19882a9c7d89ca4
Kelly Nelson <kelnel86@gmail.com>
2022-08-30 16:15:29 -0500

    displays drinks data as json in browser

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const drinks = require("./models/drinks.js");
 
 //Routes:
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
+app.get("/drinks", function(req, res){
+    res.send(drinks);
+})
+
+
+
 
 // Set up the server to listen:
 app.listen(port, () => {
